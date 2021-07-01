2ee0f221e342fc91f325e5cb897a5740dbc6269b
jose diaz <jose7312000@gmail.com>
2021-07-01 17:47:43 -0500

    Agregando imagen a la noticia especifica

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,16 +5,21 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
                 <div class="card">
-                    <h5 class="card-title m-3">{{ $post->title }}</h5>
-                    <p class="card-text m-3">
-                        {{ $post->body }}
-                    </p>
-                    <p class="text-muted m-3">
-                        <em>
-                            &ndash; {{ $post->user->name }}
-                        </em>
-                        {{ $post->created_at->format('d M Y') }}
-                    </p>
+                    <div class="card-body">
+                        @if($post->imagen)
+                        <img src="{{ $post->get_image }}" alt="imagen" class="card-img-top">
+                        <h5 class="card-title m-3">{{ $post->title }}</h5>
+                        <p class="card-text m-3">
+                            {{ $post->body }}
+                        </p>
+                        <p class="text-muted m-3">
+                            <em>
+                                &ndash; {{ $post->user->name }}
+                            </em>
+                            {{ $post->created_at->format('d M Y') }}
+                        </p>
+                      @endif
+                    </div>
                 </div>
         </div>
     </div>
